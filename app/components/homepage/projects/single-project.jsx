9802aafcf75bf36332987e778787a6efe2cdb546
sakthisaran <104762752+sakthisarans@users.noreply.github.com>
2026-02-10 17:09:07 +0530

    update

--- app/components/homepage/projects/single-project.jsx
+++ app/components/homepage/projects/single-project.jsx
@@ -4,7 +4,8 @@ import { FaCode, FaPlay } from 'react-icons/fa';
 import placeholder from '/public/png/placeholder.png';
 
 const SingleProject = ({ project }) => {
-  const { name, description, tags, code, demo, image, features } = project;
+  const { name, description, tags, tools, code, demo, image, features } = project;
+  const projectTags = tags || tools || [];
 
   return (
     <div className='group w-full h-fit flex flex-col items-center justify-center relative cursor-text overflow-hidden px-3 md:px-8 py-[1.4rem] bg-[linear-gradient(90deg,#281e57_0%,#201435_100%)] shadow-2xl rounded-lg border border-[#1a1443]'
@@ -55,7 +56,7 @@ const SingleProject = ({ project }) => {
             className="flex justify-center items-center w-10 h-10 rounded-full border-2 border-[#EFF3F4] text-[#EFF3F4] transition-all duration-300 hover:bg-[#231d4b] hover:text-violet-600 hover:border-[#0F0C41] hover:scale-110 decoration-clone cursor-pointer no-underline delay-[0.3s]">
             <FaPlay />
           </Link>
-          
+
           <Link
             href={code}
             target='_blank'
@@ -68,7 +69,7 @@ const SingleProject = ({ project }) => {
         {description}
       </p>
       <div className='group-hover:translate-x-0 absolute w-[140px] text-[0.8rem] flex justify-center gap-2 flex-col translate-x-full transition-transform duration-[0.5s] delay-[0.3s] p-[0.825rem] rounded-[10px_0_0_10px] right-0 bottom-4 bg-[#0f0b24] text-[#EFF3F4]'>
-        {tags.map((tag, id) => (
+        {projectTags.map((tag, id) => (
           <span className='font-medium break-words text-xs' key={id}>
             {tag}
           </span>
